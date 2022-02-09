10887252fd8d681475d3afbbd81ad7478a924f6d
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-02-09 15:08:52 -0300

    fix: add database title

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
 
-dotenv.config();
+dotenv.config()
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
-const db = mongoClient.db('myWallet');
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+await mongoClient.connect()
+const db = mongoClient.db('chess-store')
 
-export default db;
+export default db
